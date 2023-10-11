29742a231f311eb2155524799772061b28001b60
Amulike Kennedy <amulikekennedy@gmail.com>
2023-10-11 18:13:02 +0300

    first commit

--- App.js
+++ App.js
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className='flex-1 justify-center items-center '>
+      <Text className='text-red-500 text-lg' >Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
